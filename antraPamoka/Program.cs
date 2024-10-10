0523838493782f40ec521610ae835fe81112ab40
sKrasiuk <s.krasiukov@gmail.com>
2024-10-10 19:42:45 +0300

    projects added

---
+++ antraPamoka/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Please input a string:");
+//Console.WriteLine();
+//string input = Console.ReadLine();
+//Console.WriteLine(input[input.Length - 1]);
+//Console.WriteLine(input.Length);
+
+
+
+string st = "Mano vardas Sergej";
+string subSt = st.Substring(12);
+string subSt2 = st.Substring(0, 4);
+Console.WriteLine(subSt);
+Console.WriteLine(subSt2);
