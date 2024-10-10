9a5c1af99d191e5a0db70e4fce37df9e6fad283f
sKrasiuk <s.krasiukov@gmail.com>
2024-10-10 19:44:51 +0300

    pirma paskaita added

---
+++ pirmaPaskaita/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("   /\\");
+Console.WriteLine("  /  \\");
+Console.WriteLine(" /    \\");
+Console.WriteLine("/______\\");
+
+
+
+//Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("     *****");
+Console.WriteLine("   **     **");
+Console.WriteLine(" **         **");
+Console.WriteLine("**   Hello   **");
+Console.WriteLine("**    from   **");
+Console.WriteLine(" **   C#    **");
+Console.WriteLine("   **     **");
+Console.WriteLine("     *****");
+
+
+int amount = 5;
+int value = 2;
+string product = "Obuolis";
+
+Console.WriteLine("Preke: {0}", product);
+Console.WriteLine("Kiekis: " + amount);
+Console.WriteLine($"Kaina {value} eur");
+
+
+int age = 28;
+
+Console.WriteLine("============== VIZITINE ===============");
+Console.WriteLine("Vardas: Vardenis Pavardenis");
+Console.WriteLine("Amzius: {0}", age);
+Console.WriteLine("Pareigos: Software Developer");
+Console.WriteLine("E. Pastas: johndoe@example.com");
+Console.WriteLine("Tel.: +1 123-456-7890");
+Console.WriteLine("=======================================");
