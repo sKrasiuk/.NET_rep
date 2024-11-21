bf96bd20301c6fce04fb05a6a8ac6ebf5108c798
sKrasiuk <s.krasiukov@gmail.com>
2024-11-21 20:32:31 +0200

    Generics WIP

---
+++ _2_Kursas/7_OOP_Generic/Program.cs
@@ -0,0 +1,10 @@
+﻿using _7_OOP_Generic;
+
+//var validator = new Validation<string>();
+//validator.Validate("String");
+
+string str = null;
+//validator.Validate(str);
+//Validation<string>.Validate("string");
+
+Validation<string, int>.PrintTypesAndValues("string", 9);
---
+++ _2_Kursas/7_OOP_Generic/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace _7_OOP_Generic
+{
+    public static class Validation<T, F>
+    {
+        public static void Validate(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            else
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        public static void PrintTypesAndValues(T value, F value2)
+        {
+            Console.WriteLine("{0}: {1} | {2}: {3}", value.GetType().Name, value, value2.GetType().Name, value2);
+        }
+    }
+}
