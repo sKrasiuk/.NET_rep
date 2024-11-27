fa8d9b3d8318ba679b991ee7649ef01ff3287eec
sKrasiuk <s.krasiukov@gmail.com>
2024-11-27 22:13:55 +0200

    Exceptions handling

---
+++ _2_Kursas/10_oop_Exceptions/Program.cs
@@ -0,0 +1,50 @@
+﻿//try
+//{
+//    double x = Convert.ToDouble("str");
+//}
+//catch (FormatException e)
+//{
+//    Console.WriteLine("Invalid string format to convert to double: " + e.Message);
+//}
+
+
+
+//int[] arr = { 1, 2, 3, 4, 5 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    Console.WriteLine(arr[i]);
+//}
+
+//try
+//{
+//    Console.WriteLine(arr[7]);
+//}
+//catch (IndexOutOfRangeException e)
+//{
+//    Console.WriteLine("No such index in the array: " + e.Message);
+//}
+
+
+
+using _10_oop_Exceptions;
+
+int[] arr = { 19, 0, 75, 52 };
+for (int i = 0; i < arr.Length; i++)
+{
+    try
+    {
+        Console.WriteLine(arr[i] / arr[i + 1]);
+    }
+    catch (DivideByZeroException e)
+    {
+        Console.WriteLine("Can not divide by zero: " + e.Message);
+        continue;
+    }
+    catch (IndexOutOfRangeException e)
+    {
+        Console.WriteLine("Index out of range: " + e.Message);
+    }
+}
+
+
+ReadFile.ReadF();
---
+++ _2_Kursas/10_oop_Exceptions/ReadFile.cs
@@ -0,0 +1,31 @@
+﻿namespace _10_oop_Exceptions
+{
+    public static class ReadFile
+    {
+        private static string path = "someFile.txt";
+
+        public static void ReadF()
+        {
+            try
+            {
+                File.ReadAllText(path);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found: " + e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("No access: " + e.Message);
+            }
+            catch (PathTooLongException e)
+            {
+                Console.WriteLine("Path or filename is too long: " + e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("File in use by another application or process: " + e.Message);
+            }
+        }
+    }
+}
