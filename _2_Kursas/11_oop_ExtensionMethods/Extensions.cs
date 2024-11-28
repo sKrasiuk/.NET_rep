3c2a0990ee85fcecdf354e5110d398145945c1e9
sKrasiuk <s.krasiukov@gmail.com>
2024-11-28 22:10:50 +0200

    Extension methods

---
+++ _2_Kursas/11_oop_ExtensionMethods/Extensions.cs
@@ -0,0 +1,70 @@
+﻿namespace _11_oop_ExtensionMethods
+{
+    public static class Extensions
+    {
+        public static bool IsPositive(this int num)
+        {
+            if (num > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsLess(this int num, int num2)
+        {
+            if (num2 > num)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsContainsSpace(this string str)
+        {
+            if (str.Contains(' '))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string GenerateEmailAddress(this string name, string surname, int yearOfBirth, string domain)
+        {
+            return $"{name}{surname}{yearOfBirth}@{domain}.com".ToLower();
+        }
+
+        public static bool FindAndReturnIfEqual<T>(this List<T> list, T value, out T foundValue)
+        {
+            foundValue = default;
+            foreach (var item in list)
+            {
+                if (item.Equals(value))
+                {
+                    foundValue = value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static List<T> EveryOtherWord<T>(this List<T> list)
+        {
+            var newList = new List<T>(list.Count / 2);
+            for (int i = 0; i < list.Count; i += 2)
+            {
+                newList.Add(list[i]);
+            }
+            return newList;
+        }
+    }
+}
---
+++ _2_Kursas/11_oop_ExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using _11_oop_ExtensionMethods;
+
+int x = 2;
+string str = "string string";
+var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+x.IsPositive();
+x.IsEven();
+x.IsLess(3);
+str.IsContainsSpace();
+Extensions.GenerateEmailAddress("Vardenis", "Pavardenis", 1990, "gmail");
+
+list.EveryOtherWord();
+list.FindAndReturnIfEqual(9, out int value);
+Console.WriteLine(value);
